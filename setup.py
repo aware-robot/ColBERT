a60e7c80e8b48ab79182d45d1b31813ce28d2361
luislechugaruiz <luislechugaruiz@gmail.com>
2023-02-27 09:22:51 +0100

    Instal pip WIP

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-import setuptools
+from setuptools import setup, Extension
+import os
+import torch.utils
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-setuptools.setup(
+# Define the extension module
+torch_path = os.path.join(torch.__path__[0], 'include/')
+torch_api_path = os.path.join(torch_path, 'torch/csrc/api/include/')
+segmented_maxsim_module = Extension(
+    'colbert.modeling.segmented_maxsim',
+    sources=['colbert/modeling/segmented_maxsim.cpp'],
+    include_dirs=[torch_path, torch_api_path]
+)
+
+setup(
     name='ColBERT',
     version='0.2.0',
     author='Omar Khattab',
     author_email='okhattab@stanford.edu',
     description="Efficient and Effective Passage Search via Contextualized Late Interaction over BERT",
+    ext_modules=[segmented_maxsim_module],
+    install_requires=open('requirements.txt').read().splitlines(),
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/stanford-futuredata/ColBERT',
